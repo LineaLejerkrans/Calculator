b62b6f6de053c4241abbb57497d2e86f8b76dcf4
LineaLejerkrans <linlej020411@student.jenseneducation.se>
2021-03-18 12:59:28 +0100

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -6,107 +6,124 @@ let calc = false; // to track if you press multiple operators after each other
  
 
 function numbers(keyId){
-  
-    if (calc) // if you have done a calculation and press a new numbers you want to erase the old numbers in the array
-    {
-       input[0] = "";
-       calc = false;
+    if ((''+input[index]).length<18){ // it will only show a maximum of 18 digits
+        if (!(input[index]=='0' && keyId == '0')){ // so you can't write multipul zeros at the start
+            if (calc){ // if you have done a calculation and press a new numbers you want to erase the old numbers in the array
+                input[0] = "";
+                calc = false;
+            }
+            console.log({input});
+            input[index] = input[index] + (keyId);           
+            elem.value = input[index]; // print the numbers   
+            check();
+        }
     }
-    console.log({input});
-    input[index] = input[index] + (keyId);           
-    elem.value = input[index]; // print the numbers  
 }
- 
+
+function check(){
+    if ((''+input[index]).length>15) { 
+        elem.style.fontSize = '20px';
+    }
+    else {
+        elem.style.fontSize = '28px';
+    } 
+}
+
 function operator(keyId){
-   calc = false;
-   if (input[0] != "" && input[1] != "") { // if you have numbers in both positions than you will automatically calculate them if you were to press a new operator before clicking equal
-       calculate();
-   }
-   if (input[0] != "") { // if you only have numbers in the first position than you want to change to the other
-       index =1 ;
-   }
-   signs = keyId; // put the id of the operator in an array, it will only remember the last operator you clicked on if you were to click twice at an operator
-   console.log(signs);
+    calc = false;
+    if (input[0] != "" && input[1] != "") { // if you have numbers in both positions than you will automatically calculate them if you were to press a new operator before clicking equal
+        calculate();
+    }
+    if (input[0] != "") { // if you only have numbers in the first position than you want to change to the other
+        index =1 ;
+    }
+    signs = keyId; // put the id of the operator in an array, it will only remember the last operator you clicked on if you were to click twice at an operator
+    console.log(signs);
 }
  
 function calculate(){
-   let result = 0;
-   x = parseFloat(input[0]); // convert the array which is a string to a float
-   y = parseFloat(input[1]);
+    let result = 0;
+    x = parseFloat(input[0]); // convert the array which is a string to a float
+    y = parseFloat(input[1]);
  
-   switch(signs) {
-       case 'multiply':
-           result = x * y;
-       break;
+    switch(signs) {
+        case 'multiply':
+            result = x * y;
+        break;
  
-       case 'subtract':
-           result = x - y;
-       break;
+        case 'subtract':
+            result = x - y;
+        break;
  
-       case 'add':
-           result = x + y;
-       break;
+        case 'add':
+            result = x + y;
+        break;
  
-       case 'divide':
-           if (input[1] == "0") // if you try to divide by 0 it will show error
-           {
-               elem.value = "Error";
-           }
-           else {
-               result = x / y;
-           }
-       break;
-       }
-       if (elem.value == "Error"){
-       }
-       else{
-           elem.value = result; // display the result unless it's error
-       }
+        case 'divide':
+            if (input[1] == "0") // if you try to divide by 0 it will show error
+            {
+                elem.value = "Error";
+            }
+            else {
+                result = x / y;
+            }
+        break;
+        }
+
+        if (elem.value != "Error"){
+            if ((''+result).length>15) { 
+                elem.style.fontSize = '20px';   
+            }
+            else {
+                elem.style.fontSize = '28px';
+            }
+            elem.value = result; // display the result unless it's error
+        }
       
-       console.log(result);
-       input[0] = result.toString(); // put the result as a string in the first position
-       input[1] = ""; // and clear the second one
-       index = 0; // change the index to 0
-       calc = true; // to know if you have done a calculation
+        console.log(result);
+        input[0] = result.toString(); // put the result as a string in the first position
+        input[1] = ""; // and clear the second one
+        index = 0; // change the index to 0
+        calc = true; // to know if you have done a calculation
 }
  
  
  
 function handleClick(keyId){ //receives the id
-   if (keyId == 'dot'){
-       if (!input[index].includes(".")){ // if there is already a dot in the number you don't want to add another
-           input[index] = input[index] + '.';
-           elem.value = input[index];
-       }
-     
-   }
-   else if (keyId =='negate') {
-       input[index] = (-parseFloat(input[index])).toString(); // you convert what you have in the array to a float than make it negative and then convert it back to a string
-       elem.value = input[index];
-   }
-   else if (keyId == 'percent') {
-       input[index] = (0.01*parseFloat(input[index])).toString(); // same here only that you take the number times 0.01
-       elem.value = input[index];
-   }
-   else if (keyId == 'clear') {
-       location.reload(); // Reloads the page
-   }
-   else if (!isNaN(parseInt(keyId))){ // if what you press is a number than the numbers function will be called
-       numbers(keyId);
-   }
-   else if (keyId == 'equal'){ // if you press equal than the calculate function is called
-       calculate();
-   }
-   else { // if the keyid is neither of the ones above than it's an operator
-       operator(keyId);
-   }
+    if (keyId == 'dot'){
+        if (!input[index].includes(".")){ // if there is already a dot in the number you don't want to add another
+            input[index] = input[index] + '.';
+            elem.value = input[index];
+        }
+        
+    }
+    else if (keyId =='negate') {
+        input[index] = (-parseFloat(input[index])).toString(); // you convert what you have in the array to a float than make it negative and then convert it back to a string
+        elem.value = input[index];
+    }
+    else if (keyId == 'percent') {
+        input[index] = (0.01*parseFloat(input[index])).toString(); // same here only that you take the number times 0.01
+        elem.value = input[index];
+    }
+    else if (keyId == 'clear') {
+        location.reload(); // Reloads the page
+    }
+    else if (!isNaN(parseInt(keyId))){ // if what you press is a number than the numbers function will be called
+        numbers(keyId);
+    }
+    else if (keyId == 'equal'){ // if you press equal than the calculate function is called
+        calculate();
+    }
+    else { // if the keyid is neither of the ones above than it's an operator
+        operator(keyId);
+    }
 }
  
  
-   document.querySelectorAll(".key").forEach( //every element that has the class "key"
-   el => {
-       el.addEventListener('click', () => handleClick(el.id)) //adds a listener, that waits for a "click" and when "click"
-       //it calls the function handleClick with the id of the button you clicked on
+    document.querySelectorAll(".key").forEach( //every element that has the class "key"
+    el => {
+        el.addEventListener('click', () => handleClick(el.id)) //adds a listener, that waits for a "click" and when "click"
+        //it calls the function handleClick with the id of the button you clicked on
 });
  
  
@@ -115,7 +132,3 @@ function handleClick(keyId){ //receives the id
  
  
 // kan inte ta 2 + 2 + 2
-// ska inte kunna skriva flera nollor efter varandra
-// inte skriva in hur långa tal som helst och göra siffrorna mindre när de blir för många 
- 
- 
